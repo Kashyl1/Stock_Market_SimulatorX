e3d3b9c3ebd19f13fbd8ce51ccec954ee1ba0e2e
Jan Chodorowski <117290041+DrogerPL@users.noreply.github.com>
2024-09-20 06:14:59 +0200

    Delete Trading-Simulator/frontend directory

--- Trading-Simulator/frontend/src/components/Routing/PublicRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PublicRoute = ({ element: Component, isLoggedIn, ...rest }) => {
-  return !isLoggedIn ? <Component {...rest} /> : <Navigate to="/main" />;
-};
-
-export default PublicRoute;
