6e0d4ff00363c49d0cca759e1ac65348d27e6231
Kashyl1 <Babinskikamil8@gmail.com>
2024-11-16 11:59:31 +0100

    Fix error with mvn install

--- Trading-Simulator/backend/src/main/java/com/example/backend/portfolio/CreatePortfolioRequest.java
+++ Trading-Simulator/backend/src/main/java/com/example/backend/portfolio/CreatePortfolioRequest.java
@@ -1,12 +1,14 @@
 package com.example.backend.portfolio;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Schema(description = "Request object for creating a new portfolio")
